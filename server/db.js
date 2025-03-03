8c9206304059fcb8f826c2c5842c4985a12de606
Edrin-Mathew <edrin.mathew@bcah.christuniversity.in>
2025-03-03 15:57:06 +0530

    first

--- server/db.js
+++ server/db.js
@@ -1,12 +1,12 @@
-require('dotenv').config();  // Load environment variables
+require('dotenv').config(); 
 
 const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGO_URI;  // Read connection string from .env
+const uri = process.env.MONGO_URI;  
 
 if (!uri) {
     console.error("❌ MongoDB URI is missing. Check your .env file.");
-    process.exit(1);  // Stop execution if URI is missing
+    process.exit(1);  
 }
 
 const client = new MongoClient(uri);
